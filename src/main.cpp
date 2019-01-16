092186e665be022b42d220bdd3a0b8c5672d9c0c
tarotene <tarotene@gmail.com>
2019-01-16 13:29:29 +0900

    alternate f90 by cpp

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+struct Model {
+    ;
+};
+
+struct Job {
+    ;
+};
+
+Model defineModel();
+
+Job defineJob(Model &);
+
+int executeJob(Job &);
+
+int main(int argc, char const *argv[])
+{
+    int err = 0;
+
+    Model model = defineModel();
+
+    Job job = defineJob(model);
+
+    int err = executeJob(job);
+
+    return 0;
+}
+
+Model defineModel() {
+    ;
+}
+
+Job defineJob(Model &model) {
+    ;
+}
+
+int executeJob(Job &job) {
+    ;
+}
